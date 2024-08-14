1cec0dd22712de3108d136d63f8c87b404192cdd
Andrew Collins <collina2@uci.edu>
2024-08-13 19:51:55 -0700

    Initial Commit

---
+++ Hunt Helper/Hunt_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hunt_HelperApp.swift
+//  Hunt Helper
+//
+//  Created by Andrew Collins on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Hunt_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
